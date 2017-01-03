c6c50880e6c2d99d8ac93f6035c4def8d2c33a30
Tim Wojtulewicz <timwoj@ieee.org>
2017-01-02 17:56:32 -0700

    Shadowcraft boilerplate

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,5 @@ import ReactDOM from 'react-dom';
 import base_style from '../css/base.sass';
 import screen_style from '../css/screen.css.sass';
 
-ReactDOM.render(<Hello/>, document.getElementById('reactEntry'));
+ReactDOM.render(<Hello/>, document.getElementById('container'));
 
