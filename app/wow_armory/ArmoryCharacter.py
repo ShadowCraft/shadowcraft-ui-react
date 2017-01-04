88b6457ddbb693975b1ba83f4ab39187a1d1ca33
Tim Wojtulewicz <timwoj@ieee.org>
2017-01-03 20:28:46 -0700

    Constants plus a class for retrieving character data

---
+++ app/wow_armory/ArmoryCharacter.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+import json, csv
+from ArmoryDocument import ArmoryDocument, ArmoryError, ArmoryMissingDocument
+import ArmoryConstants
+
+class ArmoryCharacter(object):
+
+    artifact_ids = None
+
+    def __init__(self, character, realm, region='us'):
+        region = region.lower()
+        params = {'fields': 'talents,items'}
+        json = ArmoryDocument.get(region,'/wow/character/%s/%s' % (realm, character), params)
+        self.populate(region, json)
+    
+    def populate(self, region, json):
+        self.name = json['name']
+        self.level = int(json['level'])
+        self.realm = json['realm']
+        self.player_class = ArmoryConstants.CLASS_MAP[int(json['class'])]
+        self.race = ArmoryConstants.RACE_MAP[int(json['race'])]
+        self.portrait = 'http://%s.battle.net/static-render/%s/%s' % (region, region, json['thumbnail'])
+
+        # For talents, make sure to ignore any blank specs. Druids will actually have 4 specs
+        # filled in, but rogues will return three good specs and one with a blank calcSpec
+        # field.
+        self.talents = [x for x in json['talents'] if len(x['calcSpec']) > 0]
+
+        self.gear = {}
+        if 'items' not in json or len(json['items']) == 0:
+            raise ArmoryError('No items found on character')
+
+        for k,v in json['items'].items():
+            if type(v) is not dict: continue
+            if k not in ArmoryConstants.SLOT_MAP: continue
+            
+            tooltip = v['tooltipParams'] if 'tooltipParams' in v else {}
+            info = {
+                'id': v['id'],
+                'item_level': v['itemLevel'],
+                'gems': [],
+                'slot': ArmoryConstants.SLOT_MAP[k],
+                'bonuses': v['bonusLists'],
+                'context': v['context'],
+                'quality': v['quality'],
+            }
+
+            info['enchant'] = tooltip['enchant'] if 'enchant' in tooltip else 0
+            info['gems'].append(tooltip['gem0'] if 'gem0' in tooltip else 0)
+            info['gems'].append(tooltip['gem1'] if 'gem1' in tooltip else 0)
+            info['gems'].append(tooltip['gem2'] if 'gem2' in tooltip else 0)
+
+            # We squash all of the world quest contexts down into one.
+            # TODO: why are we doing this again? something about the data being the
+            # same between all of those contexts?
+            if info['context'].startswith('world-quest'):
+                info['context'] = 'world-quest'
+
+            self.gear[info['slot']] = info
+
+        # Artifact data from the API looks like this:
+        #            "artifactTraits": [{
+        #                "id": 1348,
+        #                "rank": 1
+        #            }, {
+        #                "id": 1061,
+        #                "rank": 4
+        #            }, {
+        #                "id": 1064,
+        #                "rank": 3
+        #            }, {
+        #                "id": 1066,
+        #                "rank": 3
+        #            }, {
+        #                "id": 1060,
+        #                "rank": 3
+        #            }, {
+        #                "id": 1054,
+        #                "rank": 1
+        #            }],
+        #            "relics": [{
+        #                "socket": 0,
+        #                "itemId": 133008,
+        #                "context": 11,
+        #                "bonusLists": [768, 1595, 1809]
+        #            }, {
+        #                "socket": 1,
+        #                "itemId": 133057,
+        #                "context": 11,
+        #                "bonusLists": [1793, 1595, 1809]
+        #            }],
+        self.artifact = {}
+        self.artifact['traits'] = []
+        for trait in json['items']['mainHand']['artifactTraits']:
+            # Special case around an error in the artifact power DBC data from the CDN where
+            # trait ID 859 maps to multiple spell IDs.
+            if trait['id'] == 859:
+                trait['id'] = 197241
+            else:
+                trait['id'] = ArmoryCharacter.artifact_id(trait['id'])
+            self.artifact['traits'].append(trait)
+
+        self.artifact['relics'] = []
+        for relic in json['items']['mainHand']['relics']:
+            r = {
+                'socket': relic['socket'],
+                'id': relic['itemId'],
+                'bonuses': relic['bonusLists'],
+            }
+
+            # Make another request to blizzard to get the item level for this relic,
+            # since the character data doesn't include enough information.
+            try:
+                params = {'bl': ','.join(map(str,relic['bonusLists']))}
+                relic_json = ArmoryDocument.get(region,'/wow/item/%d' % relic['itemId'], params)
+                r['ilvl'] = relic_json['itemLevel']
+                self.artifact['relics'].append(r)
+            except ArmoryMissingDocument:
+                print("Failed to retrieve extra relic data")
+
+    # Maps the a trait ID from the artifact data to a spell ID using the DBC data
+    # from the Blizzard CDN
+    def artifact_id(trait_id):
+        # The header on the ArtifactPowerRank data looks like (as of 7.0.3):
+        # id,id_spell,value,id_power,f5,index
+        # We're mapping between id_power and id_spell
+        if ArmoryCharacter.artifact_ids == None:
+            ArmoryCharacter.artifact_ids = {}
+            skip = True
+            # TODO: this should really look up the location of this path in some other manner
+            with open('../external_data/ArtifactPowerRank.dbc.csv', mode='r') as infile:
+                reader = csv.reader(infile)
+                for row in reader:
+                    if skip:
+                        # skip the first row with the header
+                        skip = False
+                    else:
+                        ArmoryCharacter.artifact_ids[int(row[3])] = int(row[1])
+        return ArmoryCharacter.artifact_ids[trait_id] if trait_id in ArmoryCharacter.artifact_ids else 0
+                  
+if __name__ == '__main__':
+
+    character = ArmoryCharacter('tamen','aerie-peak','us')
+    print(character.artifact)
---
+++ app/wow_armory/ArmoryConstants.py
@@ -0,0 +1,182 @@
+BONUS_ACTIONS = {
+    1: "ADJUST_ITEM_LEVEL",
+    2: "MODIFY_STATS",
+    3: "CHANGE_ITEM_QUALITY",
+    4: "ADD_ITEM_TITLES",
+    5: "APPEND_WORDS_TO_ITEM_NAME",
+    6: "ADD_SOCKETS",
+    7: "ADJUST_ITEM_APPEARANCE_ID",
+    8: "ADJUST_EQUIP_LEVEL",
+    9: "UNKNOWN_1",
+    10: "UNKNOWN_2",
+    14: "BASE_ITEM_LEVEL",
+}
+
+ITEM_BONUS_TYPES = {
+    'ilvl_increase': 1,
+    'random_stat': 2,
+    'quality': 3,
+    'title': 4,
+    'name': 5,
+    'socket': 6,
+    'base_ilvl': 14
+}
+
+ENCHANT_SCALING = 10.0 # for lvl 90 // lvl100 = 80
+
+CLASS_MAP = {
+    4: 'rogue'
+}
+
+RACE_MAP = {
+    1: 'Human',
+    2: 'Orc',
+    3: 'Dwarf',
+    4: 'Night Elf',
+    5: 'Undead',
+    6: 'Tauren',
+    7: 'Gnome',
+    8: 'Troll',
+    9: 'Goblin',
+    10: 'Blood Elf',
+    11: 'Draenei',
+    22: 'Worgen',
+    24: 'Pandaren',
+    25: 'Pandaren',
+    26: 'Pandaren'
+}
+
+SLOT_MAP = {
+    'head': 0,
+    'neck': 1,
+    'shoulder': 2,
+    'back': 14,
+    'chest': 4,
+    'wrist': 8,
+    'hands': 9,
+    'waist': 5,
+    'legs': 6,
+    'feet': 7,
+    'finger1': 10,
+    'finger2': 11,
+    'trinket1': 12,
+    'trinket2': 13,
+    'mainHand': 15,
+    'offHand': 16,
+}
+
+# TODO: missing some mappings in here based on data from ItemBonus.dbc. The follow
+# types are missing: 7, 9, 10, 11, 12, 13, 17, 18 (these are probably relics).
+SOCKET_MAP = {
+    1: 'Meta',
+    2: 'Red',
+    8: 'Blue',
+    4: 'Yellow',
+    14: 'Prismatic',
+    16: 'Hydraulic',
+    32: 'Cogwheel'
+}
+
+STAT_LOOKUP = {
+    1: "health",
+    2: "mana",
+    3: "agility",
+    4: "strength",
+    5: "intellect",
+    6: "spirit",
+    7: "stamina",
+    12: "defense",
+    13: "dodge",
+    14: "parry",
+    15: "shield_block",
+    31: "hit",
+    32: "crit",
+    33: "hit_avoidance",
+    34: "critical_strike_avoidance",
+    35: "pvp_resilience",
+    36: "haste",
+    37: "expertise",
+    38: "attack_power",
+    40: "versatility",
+    41: "damage_done",
+    42: "healing_done",
+    43: "mana_every_5_seconds",
+    44: "armor_penetration",
+    45: "power",
+    46: "health_every_5_seconds",
+    47: "penetration",
+    48: "block_value",
+    49: "mastery",
+    50: "bonus_armor",
+    57: "pvp_power",
+    58: "amplify",
+    59: "multistrike",
+    61: "speed",
+    62: "leech",
+    63: "avoidance",
+    64: "indestructible",
+    67: "versatility",
+    71: "agility",
+    72: "agility",
+    73: "agility",
+}
+
+WOWHEAD_STAT_MAP = {
+    'hitrtng': 'hit',
+    'hastertng': 'haste',
+    'critstrkrtng': 'crit',
+    'mastrtng': 'mastery',
+    'exprtng': 'expertise',
+    'agi': 'agility',
+    'sta': 'stamina',
+    'pvppower': 'pvp_power',
+    'resirtng': 'pvp_resilience',
+    'multistrike': 'multistrike',
+    'versatility': 'versatility'
+}
+
+ARMOR_CLASS = {
+    0: 'None',
+    1: 'Cloth',
+    2: 'Leather',
+    3: 'Mail',
+    4: 'Plate'
+}
+
+GEM_SUBCLASS_MAP = {
+    0: 'Red',
+    1: 'Blue',
+    2: 'Yellow',
+    3: 'Purple',
+    4: 'Green',
+    5: 'Orange',
+    6: 'Meta',
+    7: "Simple",
+    8: 'Prismatic',
+    9: 'Hydraulic',
+    10: 'Cogwheel'
+}
+
+SCAN_ATTRIBUTES = ['agility', 'strength', 'intellect', 'spirit',
+                   'stamina', 'attack power', 'critical strike',
+                   'versatility', 'multistrike', 'haste', 'mastery',
+                   'pvp resilience', 'pvp power', 'all stats',
+                   'dodge', 'block', 'parry']
+
+SCAN_OVERRIDE = {
+    'critical strike': 'crit',
+}
+
+RELIC_TYPE_MAP = {
+    'Iron': 0,
+    'Blood': 1,
+    'Shadow': 2,
+    'Fel': 3,
+    'Arcane': 4,
+    'Frost': 5,
+    'Fire': 6,
+    'Water': 7,
+    'Life': 8,
+    'Storm': 9,
+    'Holy': 10,
+}
