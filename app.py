865f58523957b33f2d09af596c41f8904e99c0ca
Tim Wojtulewicz <timwoj@ieee.org>
2017-01-13 08:47:44 -0700

    pylint app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect
 from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
 from werkzeug.routing import BaseConverter
 import shadowcraft_ui
-import os
 
 app = Flask('shadowcraft_ui')
 app.config['SECRET_KEY'] = 'shhhhhhhh!'
@@ -16,16 +15,16 @@ class RegexConverter(BaseConverter):
         super(RegexConverter, self).__init__(url_map)
         self.regex = items[0]
 app.url_map.converters['regex'] = RegexConverter
-        
+
 @app.route('/')
 def main():
-  return render_template('index.html')
+    return render_template('index.html')
 
 # Main route for the application. Loads a character.
 @app.route('/<regex("(us|eu|kr|tw|cn|sea)"):region>/<realm>/<name>')
 def character_show(region, realm, name):
     data = shadowcraft_ui.get_character_data(mongo, region, realm, name)
-    return "data"
+    return data
 
 # Refreshes a character from the armory and redirects to the main route.
 # TODO: Flask adds a "redirecting" page before redirecting. Is there a way
@@ -39,17 +38,19 @@ def character_refresh(region, realm, name):
 # Requests a character page based on a saved sha value.
 @app.route('/<regex("(us|eu|kr|tw|cn|sea)"):region>/<realm>/<name>/#!/<sha>')
 def character_sha(region, realm, name, sha):
-    shadowcraft_ui.refresh_character(mongo, region, realm, name)
+    shadowcraft_ui.get_character_data(mongo, region, realm, name, sha=sha)
     url = url_for('character_show', region=region, realm=realm, name=name)
     return redirect(url)
 
 # TODO: are these really necessary? Can't we just return 400/500 errors when
 # necessary and configure flask to handle them as such?
 @app.route('/error')
-def error(): return render_template('500.html')
+def error():
+    return render_template('500.html')
 
 @app.route('/missing')
-def missing(): return render_template('404.html')
+def missing():
+    return render_template('404.html')
 
 # Websocket event for requesting a new debug SHA based on from character data.
 @socketio.on('get_sha')
@@ -68,4 +69,4 @@ def get_items_by_slot(slot, min_ilvl=-1, max_ilvl=-1):
 # websocket event should be moved to one.
 
 if __name__ == '__main__':
-  socketio.run(app, debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
