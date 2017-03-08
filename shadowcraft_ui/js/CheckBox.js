3022ac44a880a9776f41b024238aec4aa51721e8
wavefunctionp <m.corey.thompson@gmail.com>
2017-03-07 23:48:01 -0600

    - attempt to handle checkbox (does not work)

--- shadowcraft_ui/js/CheckBox.js
+++ shadowcraft_ui/js/CheckBox.js
@@ -2,24 +2,20 @@ import React from 'react';
 import store from './store';
 
 export default class CheckBox extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.selectOption = this.selectOption.bind(this);
-    }
-
-    selectOption(e) {
+    onChange(e) {
         e.preventDefault();
-        store.dispatch({type: 'CHANGE_SETTING',
-                        setting: e.currentTarget.id,
-                        value: e.currentTarget.value});
+        store.dispatch({
+            type: 'CHANGE_SETTING',
+            setting: e.currentTarget.id,
+            value: (this.props.value) ? false : true
+        });
     }
 
     render() {
         return (
             <label className="select" htmlFor="opt-buffs-flask_legion_agi">
                 <span className="label">{this.props.data.label}</span>
-                <input className="optionCheck" data-ns="buffs" id={this.props.section+"."+this.props.data.name} type="checkbox" defaultChecked="checked" value={this.props.value} onChange={this.selectOption} />
+                <input className="optionCheck" id={`${this.props.section}.${this.props.data.name}`} type="checkbox" onChange={this.onChange.bind(this)} {... (this.props.value && {checked: 'checked'})} />
                 <span className="desc">{this.props.data.description}</span>
             </label>
         );
