7b31b3c7b90bfa943ef1d9782ea1f8866726acef
Tim Wojtulewicz <timwoj@ieee.org>
2017-03-23 18:44:01 -0700

    Plumb engine info into the advanced tab

--- shadowcraft_ui/js/advanced/AdvancedPane.js
+++ shadowcraft_ui/js/advanced/AdvancedPane.js
@@ -24,7 +24,8 @@ class AdvancedPane extends React.Component {
                         <h3>Version Info</h3>
                         <div className="inner">
                             <div className="stat"> <span className="key">UI </span> <span className="val">{this.props.ui_build}</span> </div>
-                            <div className="stat"> <span className="key">Engine </span> <span className="val">{this.props.engine_build}</span> </div>
+                            <div className="stat"> <span className="key">Engine Build Target</span> <span className="val">{this.props.engine_info.wow_build_target}</span> </div>
+                            <div className="stat"> <span className="key">Engine Shadowcraft Build</span> <span className="val">{this.props.engine_info.shadowcraft_build}</span> </div>
                         </div>
                     </section>
                 </div>
@@ -41,7 +42,7 @@ const mapStateToProps = function (store) {
     return {
         active_spec: store.character.active,
         ui_build: store.engine.ui_build,
-        engine_build: store.engine.build,
+        engine_info: store.engine.engine_info,
         breakdown: store.engine.breakdown,
         dps: store.engine.totalDps,
         layout: store.settings.layout,
