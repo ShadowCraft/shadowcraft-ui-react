cfb7b66145ca5e8ebff7785ff9aadbc8752b1ff4
wavefunctionp <m.corey.thompson@gmail.com>
2017-04-13 04:46:56 -0500

    gear selection popup window implimented

--- shadowcraft_ui/js/gear/EquippedItem.js
+++ shadowcraft_ui/js/gear/EquippedItem.js
@@ -4,6 +4,13 @@ import EquippedEnchant from './EquippedEnchant';
 
 export default class EquippedItem extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            modal: false
+        };
+    }
+
     IsEnchantable(slot) {
         switch (slot) {
             case 'neck': return true;
@@ -14,45 +21,97 @@ export default class EquippedItem extends React.Component {
         }
     }
 
+    onClick() {
+        this.setState({ modal: !this.state.modal });
+    }
+
     render() {
         let item = this.props.item;
         // console.log(item)
         return (
             // do we need all these data targets?
-            <div
-                className="slot"
-                data-bonus={item.bonusids}
-                data-context={item.context}
-                data-name={item.name}
-                data-quality={item.quality}
-                data-slot={item.slot}
-                data-id={item.id}
-            >
-                <div className="image">
-                    <img src={`http://media.blizzard.com/wow/icons/56/${item.icon}.jpg`} />
-                    <span className="ilvl">{item.item_level}</span>
-                </div>
-                <div className="lock lock_off">
-                    <img src="/static/images/lock_off.png" />
-                </div>
+            <div>
                 <div
-                    className="name quality-4 tt"
-                    data-tooltip-bonus={item.bonuses}
-                    data-tooltip-gems={item.gems}
-                    data-tooltip-id={item.id}
+                    className="slot"
+                    data-bonus={item.bonusids}
+                    data-context={item.context}
+                    data-name={item.name}
+                    data-quality={item.quality}
+                    data-slot={item.slot}
+                    data-id={item.id}
                 >
-                    {item.name}
-                    <em className="heroic">TODO: bonus text</em>
-                    <a className="wowhead" href={`http://legion.wowhead.com/item=${item.id}`} target="_blank">Wowhead</a>
+                    <div className="image">
+                        <img src={`http://media.blizzard.com/wow/icons/56/${item.icon}.jpg`} />
+                        <span className="ilvl">{item.item_level}</span>
+                    </div>
+                    <div className="lock lock_off">
+                        <img src="/static/images/lock_off.png" />
+                    </div>
+                    <div
+                        className="name quality-4 tt"
+                        data-tooltip-bonus={item.bonuses}
+                        data-tooltip-gems={item.gems}
+                        data-tooltip-id={item.id}
+                        onClick={this.onClick.bind(this)}
+                    >
+                        {item.name}
+                        <em className="heroic">TODO: bonus text</em>
+                        <a className="wowhead" href={`http://legion.wowhead.com/item=${item.id}`} target="_blank">Wowhead</a>
+                    </div>
+                    <div className="bonuses">
+                        {/*this probably doesn't need a huge full length div, maybe a gear under the item icon instead?'*/}
+                        <img alt="Reforge" src="/static/images/reforge.png" />Modify Bonuses</div>
+                    {/*need to pass whole item because we need to check item quality to filter out relics*/}
+                    <EquippedGemList gems={item.gems} />
+                    {/*javascript trickery to only show enchants for neck, ring and back*/}
+                    {this.IsEnchantable(item.slot) && <EquippedEnchant enchantID={item.enchant} />}
+                </div >
+                {this.state.modal ? <SelectItem /> : <div />}
+            </div>
+        );
+    }
+}
+
+class SelectItem extends React.Component {
+    render() {
+        return (
+            <div className="alternatives popup ui-dialog visible" id="gearpopup" style={{ top: '217px', left: '617px' }}>
+                <div id="filter">
+                    <input className="search" placeholder="Filter..." type="search" />
                 </div>
-                <div className="bonuses">
-                    {/*this probably doesn't need a huge full length div, maybe a gear under the item icon instead?'*/}
-                    <img alt="Reforge" src="/static/images/reforge.png" />Modify Bonuses</div>
-                {/*need to pass whole item because we need to check item quality to filter out relics*/}
-                <EquippedGemList gems={item.gems} />
-                {/*javascript trickery to only show enchants for neck, ring and back*/}
-                { this.IsEnchantable(item.slot) && <EquippedEnchant enchantID={item.enchant} />}
-            </div >
+                <div className="body">
+                    <div
+                        className="slot"
+                        data-bonus="3518"
+                        data-context=""
+                        data-identifier="140889:905"
+                        data-name="Bracers of Impossible Choices"
+                        data-quality="4"
+                        data-search="Bracers%20of%20Impossible%20Choices%20undefined"
+                        data-slot=""
+                        data-tag=""
+                        data-upgrade=""
+                        id="140889"
+                    >
+                        <div className="image">  <img src="http://us.media.blizzard.com/wow/icons/56/inv_leather_raidroguemythic_q_01bracer.jpg" />
+                            <span className="ilvl" />
+                        </div>
+                        <div
+                            className="name quality-4 tt"
+                            data-tooltip-bonus="3518"
+                            data-tooltip-gems=""
+                            data-tooltip-id="140889"
+                            data-tooltip-spec=""
+                            data-tooltip-upgd=""
+                        >
+                            <em className="ilvl">(905)</em>  Bracers of Impossible Choices  <em className="heroic" />  <a className="wowhead" href="http://legion.wowhead.com/item=140889" target="_blank">Wowhead</a>
+                        </div>
+                        <div className="gems" />
+                        <span className="tags"> Mythic</span>
+                        <span className="desc"> 2076.8 base   </span>
+                        <span className="pct"><div className="label">2076.8</div><div className="pct-inner" style={{ width: '100%' }} /></span></div>
+                </div>
+            </div>
         );
     }
 }
--- shadowcraft_ui/js/gear/GearPane.js
+++ shadowcraft_ui/js/gear/GearPane.js
@@ -7,7 +7,7 @@ export default class GearPane extends React.Component {
         //  console.log(data.gear)
         return (
             <div className="with-tools ui-tabs-panel ui-widget-content ui-corner-bottom" id="gear">
-                <StatPanel stats={this.props.data.stats} weights={this.props.data.weights}/>
+                <StatPanel stats={this.props.data.stats} weights={this.props.data.weights} />
                 {/*this layout method could probably be revised*/}
                 <div className="panel-content">
                     <div className="slots half" id="slots-left">
@@ -31,13 +31,51 @@ export default class GearPane extends React.Component {
                         <EquippedItem item={this.props.data.gear.trinket1} />
                         <EquippedItem item={this.props.data.gear.trinket2} />
                     </div>
-                    <div className="popup ui-dialog" id="bonuses">Add item bonus</div>
-                    <div className="alternatives popup ui-dialog" id="gearpopup">
-                        <div id="filter">
-                            <input className="search" placeholder="Filter..." type="search" />
+                </div >
+            </div>
+        );
+    }
+}
+
+
+class SelectItem extends React.Component {
+    render() {
+        return (
+            <div className="alternatives popup ui-dialog visible" id="gearpopup" style={{ top: '217px', left: '617px' }}>
+                <div id="filter">
+                    <input className="search" placeholder="Filter..." type="search" />
+                </div>
+                <div className="body">
+                    <div
+                        className="slot"
+                        data-bonus="3518"
+                        data-context=""
+                        data-identifier="140889:905"
+                        data-name="Bracers of Impossible Choices"
+                        data-quality="4"
+                        data-search="Bracers%20of%20Impossible%20Choices%20undefined"
+                        data-slot=""
+                        data-tag=""
+                        data-upgrade=""
+                        id="140889"
+                    >
+                        <div className="image">  <img src="http://us.media.blizzard.com/wow/icons/56/inv_leather_raidroguemythic_q_01bracer.jpg" />
+                            <span className="ilvl" />
                         </div>
-                        <div className="body" />
-                    </div>
+                        <div
+                            className="name quality-4 tt"
+                            data-tooltip-bonus="3518"
+                            data-tooltip-gems=""
+                            data-tooltip-id="140889"
+                            data-tooltip-spec=""
+                            data-tooltip-upgd=""
+                        >
+                            <em className="ilvl">(905)</em>  Bracers of Impossible Choices  <em className="heroic" />  <a className="wowhead" href="http://legion.wowhead.com/item=140889" target="_blank">Wowhead</a>
+                        </div>
+                        <div className="gems" />
+                        <span className="tags"> Mythic</span>
+                        <span className="desc"> 2076.8 base   </span>
+                        <span className="pct"><div className="label">2076.8</div><div className="pct-inner" style={{ width: '100%' }} /></span></div>
                 </div>
             </div>
         );
