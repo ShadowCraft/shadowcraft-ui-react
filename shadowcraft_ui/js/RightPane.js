78d41cf532c7268b5678f1e26e5c63c63b8c6890
Tim Wojtulewicz <timwoj@ieee.org>
2017-07-25 19:27:18 -0700

    Hook up the warnings reducer to the UI

--- shadowcraft_ui/js/RightPane.js
+++ shadowcraft_ui/js/RightPane.js
@@ -83,40 +83,45 @@ class RightPane extends React.Component {
             dpsChange = Math.round((this.props.history.dps[historyLength-1] - this.props.history.dps[historyLength-2]) * 100.0) / 100.0;
         }
 
+        let warnings = this.props.warnings.map((g, i) =>
+            <div className="log warning" key={i}>{this.props.warnings[i]}</div>);
+
         return (
             <div style={{ flex: 1}}>
                 <div id="console-footer" className="awin-medium">
-                  <a className="pandaren" href={`http://${this.props.region}.battle.net/wow/en/character/${this.props.realm}/${this.props.name}/advanced`} id="card" target="_blank">
-                    <div className="img">
-                      <img src={this.props.portrait}/>
+                    <a className="pandaren" href={`http://${this.props.region}.battle.net/wow/en/character/${this.props.realm}/${this.props.name}/advanced`} id="card" target="_blank">
+                        <div className="img">
+                            <img src={this.props.portrait}/>
+                        </div>
+                        <span className="info">
+                            <span className="name">
+                                {this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)}
+                            </span>
+                            <span className="realm">
+                                {realm} - {this.props.region.toUpperCase()}
+                            </span>
+                        </span>
+                    </a>
+                    <div id="dps">
+                        <div className="inner">{Math.round(this.props.dps*10)/10.0} {dpsChange < 0.0 ? (<em className="n">({dpsChange})</em>):(<em className="p">(+{dpsChange})</em>)}</div>
                     </div>
-                    <span className="info">
-                      <span className="name">
-                        {this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)}
-                      </span>
-                      <span className="realm">
-                        {realm} - {this.props.region.toUpperCase()}
-                      </span>
-                    </span>
-                  </a>
-                  <div id="dps">
-                      <div className="inner">{Math.round(this.props.dps*10)/10.0} {dpsChange < 0.0 ? (<em className="n">({dpsChange})</em>):(<em className="p">(+{dpsChange})</em>)}</div>
-                  </div>
-                  <Line data={graphTestData} options={graphOptions} getElementsAtEvent={this.graphClick} />
+                    <Line data={graphTestData} options={graphOptions} getElementsAtEvent={this.graphClick} />
                 </div>
                 <div id="logs">
-                  <section>
-                    <div className="window" id="console">
-                      <h3>Notices</h3>
-                      <div className="inner"><div className="log items warning" id="warn"> <span className="quality-4"> Band of Crystalline Bone </span> needs an enchantment  </div> </div>
-                    </div>
-                  </section>
-                  <section>
-                    <div className="window" id="log">
-                      <h3>Log</h3>
-                      <div className="inner" />
-                    </div>
-                  </section>
+                    <section>
+                        <div className="window" id="console">
+                            <h3>Notices</h3>
+                            <div className="inner">
+                                {warnings}
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="window" id="log">
+                            <h3>Log</h3>
+                            <div className="inner" />
+                        </div>
+                    </section>
                 </div>
             </div>
         );
