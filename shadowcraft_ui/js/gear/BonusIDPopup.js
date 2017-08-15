c579babe83bf117848a840d49e700c0416f71c24
Tim Wojtulewicz <timwoj@ieee.org>
2017-08-14 20:07:20 -0700

    Show obliterum ilvls for crafted items on bonus popup. Fixes #53

--- shadowcraft_ui/js/gear/BonusIDPopup.js
+++ shadowcraft_ui/js/gear/BonusIDPopup.js
@@ -60,7 +60,8 @@ class BonusIDPopup extends React.Component {
         this.onApply = this.onApply.bind(this);
 
         for (let idx in props.item.bonuses) {
-            if (props.item.bonuses[idx] >= 1472 && props.item.bonuses[idx] <= 1672) {
+            if ((props.item.bonuses[idx] >= 1472 && props.item.bonuses[idx] <= 1672) ||
+                (props.item.bonuses[idx] >= 669 && props.item.bonuses[idx] <= 679)) {
                 this.state.wfBonus = props.item.bonuses[idx];
                 break;
             }
@@ -88,21 +89,34 @@ class BonusIDPopup extends React.Component {
         }
         else {
             let itemdata = { chance_bonus_lists: staticItems[0]['chance_bonus_lists'],
-                             stats: null, item_level: 0, name: staticItems[0]['name'] };
+                             stats: null, item_level: 0, name: staticItems[0]['name'],
+                             is_crafted: false }
 
             let itemlevels = Object.keys(staticItems[0]['ilvls']).sort();
+
+            if ('is_crafted' in staticItems[0]) {
+                itemdata['is_crafted'] = staticItems[0]['is_crafted'];
+            }
 
             // Quickly loop through the bonus IDs on the equipped item and see if there's one that's
             // an item level increase. If there is, see if there's a perfect match for the item's
             // actual base item level.
             for (let i = 0; i < this.state.active.length; i++) {
+                let actualBase = 0;
+                let index = -1;
+
                 if (this.state.active[i] >= 1472 && this.state.active[i] <= 1672) {
-                    let actualBase = this.props.item.item_level - (this.state.active[i] - 1472);
-                    let index = itemlevels.indexOf(actualBase.toString());
-                    if (index != -1) {
-                        itemdata['item_level'] = actualBase;
-                        itemdata['stats'] = staticItems[0]['ilvls'][actualBase]['stats'];
-                    }
+                    actualBase = this.props.item.item_level - (this.state.active[i] - 1472);
+                    index = itemlevels.indexOf(actualBase.toString());
+                }
+                else if (this.state.active[i] >= 669 && this.state.active[i] <= 679) {
+                    actualBase = this.props.item.item_level - ((this.state.active[i] - 669) * 5);
+                    index = itemlevels.indexOf(actualBase.toString());
+                }
+
+                if (index != -1) {
+                    itemdata['item_level'] = actualBase;
+                    itemdata['stats'] = staticItems[0]['ilvls'][actualBase]['stats'];
                 }
             }
 
@@ -179,7 +193,7 @@ class BonusIDPopup extends React.Component {
         let eventData = {
             slot: this.props.item.slot,
             bonuses: this.state.active,
-            newIlvl: this.state.baseItem.item_level,
+            ilvl: this.state.baseItem.item_level,
             canHaveBonusSocket: this.state.baseItem.chance_bonus_lists.indexOf(1808) != -1,
             hasBonusSocket: this.state.active.indexOf(1808) != -1,
             newStats: this.state.baseItem.stats,
@@ -188,32 +202,30 @@ class BonusIDPopup extends React.Component {
         };
 
         if (this.state.wfBonus > 0) {
-            eventData['newIlvl'] += this.state.wfBonus - 1472;
+            if (this.state.baseItem.is_crafted) {
+                eventData['ilvl'] += (this.state.wfBonus - 669) * 5;
+            }
+            else {
+                eventData['ilvl'] += this.state.wfBonus - 1472;
+            }
         }
 
         if (this.state.suffixBonus > 0) {
             let propMultiplier = this.getRandPropMultiplier(this.props.item);
-            console.log(RANDOM_SUFFIX_MAP[this.state.suffixBonus].stats);
-            console.log(propMultiplier);
             for (let stat in RANDOM_SUFFIX_MAP[this.state.suffixBonus].stats) {
-                console.log(RANDOM_SUFFIX_MAP[this.state.suffixBonus].stats[stat]);
                 eventData['newStats'][stat] = RANDOM_SUFFIX_MAP[this.state.suffixBonus].stats[stat] * propMultiplier;
             }
 
             eventData['suffix'] = RANDOM_SUFFIX_MAP[this.state.suffixBonus].name;
         }
 
-        if (eventData['newIlvl'] != this.state.baseItem.item_level) {
-            console.log("updating stats for ilvl");
-            console.log(eventData['newStats']);
-            console.log(eventData['newIlvl']);
-            console.log(this.state.baseItem.item_level);
+        if (eventData['ilvl'] != this.state.baseItem.item_level) {
             eventData['newStats'] = recalculateStats(
                 Object.assign({}, eventData['newStats']),
-                (eventData['newIlvl'] - this.state.baseItem.item_level).toFixed(2));
+                (eventData['ilvl'] - this.state.baseItem.item_level).toFixed(2));
         }
 
-        console.log(eventData['newStats']);
+        console.log(eventData);
 
         store.dispatch(updateCharacterState('CHANGE_BONUSES', eventData));
         store.dispatch({type: "CLOSE_MODAL"});
@@ -296,7 +308,7 @@ class BonusIDPopup extends React.Component {
                         selectedWFBonus = 3530;
                     }
                 }
-                else {
+                else if (!this.state.baseItem.is_crafted) {
                     for (let i = 955; i >= this.state.baseItem.item_level + 5; i -= 5) {
                         let bonus = i - this.state.baseItem.item_level + 1472;
                         if (this.state.active.indexOf(bonus) != -1) {
@@ -306,9 +318,23 @@ class BonusIDPopup extends React.Component {
                         wfOptions.push(<option value={bonus} key={bonus}>Item Level {i} / +{i - this.state.baseItem.item_level}</option>);
                     }
                 }
+                else {
+                    // Obliterum items range from 0/10 to 10/10, with bonus IDs 669 and 679 respectively.
+                    for (let bonus = 679; bonus >= 669; bonus -= 1) {
+
+                        let itemLevelDiff = (bonus - 669) * 5;
+                        if (this.state.active.indexOf(bonus) != -1) {
+                            selectedWFBonus = bonus;
+                        }
+
+                        wfOptions.push(<option value={bonus} key={bonus}>Obliterum {bonus-669}/10 - Item Level {850+itemLevelDiff} / +{itemLevelDiff}</option>);
+                    }
+                }
             }
 
-            wfOptions.push(<option value="0" key="0">Item Level {this.state.baseItem.item_level} / None</option>);
+            if (!this.state.baseItem.is_crafted) {
+                wfOptions.push(<option value="0" key="0">Item Level {this.state.baseItem.item_level} / None</option>);
+            }
 
             let suffixOptions = [];
             let selectedSuffix = 0;
