539c7de5c9dc3bb06fd8be35300e2f43241dfcc8
Tim Wojtulewicz <timwoj@ieee.org>
2017-08-29 19:43:16 -0700

    Enable hashed output from webpack, including template fixes for cache-busting. Fixes #35

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,50 @@
-var webpack = require('webpack');  
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var pkg = require('./package.json');
 
-module.exports = {  
-  entry: [
-    "./shadowcraft_ui/js/app.js"
-  ],
-  output: {
-    path: __dirname + '/shadowcraft_ui/static',
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.sass$/,
-        loader: ExtractTextPlugin.extract('css!sass')
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('css/main.css', {
-      allChunks: true
+var bundleCss = ('production' === process.env.NODE_ENV) ? 'css/main-[hash:6].css' : 'css/main.css'
+var pluginsWebpack = [
+    new ExtractTextPlugin(bundleCss),
+    new HtmlWebpackPlugin({
+        filename: '../templates/index.html',
+        template: 'shadowcraft_ui/templates/webpack-index.ejs',
+        cache: true,
+        inject: false
     })
-  ]
+];
+
+if ('production' === process.env.NODE_ENV) {
+    var prodEnv = [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.DedupePlugin()
+    ]
+
+    pluginsWebpack.concat(pluginsWebpack, prodEnv);
+}
+
+module.exports = {
+    entry: './shadowcraft_ui/js/app.js',
+    output: {
+        path: __dirname + '/shadowcraft_ui/static',
+        filename: ('production' === process.env.NODE_ENV) ? 'bundle-[hash:6].js' : 'bundle.js'
+    },
+    babel: {
+        presets: ['es2015', 'react']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract('css!sass')
+            },
+        ]
+    },
+    plugins: pluginsWebpack
 };
