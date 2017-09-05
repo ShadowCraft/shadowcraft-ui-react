14375d5cc65bdd18ec19b659ace1b20365a77aee
wavefunctionp <m.corey.thompson@gmail.com>
2017-09-05 14:10:22 -0500

    Proptypes from ArtifactRelicSelect and ArtifactTrait

--- shadowcraft_ui/js/artifact/ArtifactRelicSelect.js
+++ shadowcraft_ui/js/artifact/ArtifactRelicSelect.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class ArtifactRelicSelect extends React.Component {
+class ArtifactRelicSelect extends React.Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
+        this.props = props;
         this.handleRelicChange = this.handleRelicChange.bind(this);
         this.handleIlvlChange = this.handleIlvlChange.bind(this);
     }
@@ -21,31 +22,42 @@ export default class ArtifactRelicSelect extends React.Component {
         this.props.parent.change_relic(this.props.index, this.selected_trait, this.selected_ilvl);
     }
 
-    render()
-    {
+    render() {
         this.selected_trait = this.props.selected.id;
         this.selected_ilvl = this.props.selected.ilvl;
 
-        var relics = this.props.relics.map(function(relic) {
-            return <option key={relic[0]} id={'relic-'+relic[0]+'-select'} value={relic[0]}>{relic[1]}</option>;
+        var relics = this.props.relics.map(function (relic) {
+            return <option key={relic[0]} id={'relic-' + relic[0] + '-select'} value={relic[0]}>{relic[1]}</option>;
         });
 
         var ilvls = [];
         for (var i = 835; i <= 955; i += 5) {
-            ilvls.push(<option key={i} id={'relicilvl-'+this.props.index+'-'+i} value={i}>{i}</option>);
+            ilvls.push(<option key={i} id={'relicilvl-' + this.props.index + '-' + i} value={i}>{i}</option>);
         }
 
         return (
             <label className="select">
-                <span className="label">Relic {parseInt(this.props.index)+1} ({this.props.type}):</span>
+                <span className="label">Relic {parseInt(this.props.index) + 1} ({this.props.type}):</span>
                 <span className="select-container">
-                    <select className="optionSelect" id={'relic-'+this.props.index+'-select'} data-index={this.props.index} value={this.props.selected.id} onChange={this.handleRelicChange}>
-                        <option id={'relic-'+this.props.index+'-none'} value="0">None</option>
+                    <select
+                        className="optionSelect"
+                        id={'relic-' + this.props.index + '-select'}
+                        data-index={this.props.index}
+                        value={this.props.selected.id}
+                        onChange={this.handleRelicChange}
+                    >
+                        <option id={'relic-' + this.props.index + '-none'} value="0">None</option>
                         {relics}
                     </select>
                 </span>
                 <span className="select-container">
-                    <select className="optionSelect" id={'relic-'+this.props.index+'-select'} data-index={this.props.index} value={this.props.selected.ilvl} onChange={this.handleIlvlChange}>
+                    <select
+                        className="optionSelect"
+                        id={'relic-' + this.props.index + '-select'}
+                        data-index={this.props.index}
+                        value={this.props.selected.ilvl}
+                        onChange={this.handleIlvlChange}
+                    >
                         {ilvls}
                     </select>
                 </span>
@@ -53,3 +65,18 @@ export default class ArtifactRelicSelect extends React.Component {
         );
     }
 }
+
+ArtifactRelicSelect.propTypes = {
+    parent: PropTypes.shape({
+        change_relic: PropTypes.func.isRequired
+    }).isRequired,
+    selected: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        ilvl: PropTypes.number.isRequired
+    }).isRequired,
+    index: PropTypes.string.isRequired,
+    relics: PropTypes.arrayOf(PropTypes.array.isRequired).isRequired,
+    type: PropTypes.string.isRequired
+};
+
+export default ArtifactRelicSelect;
--- shadowcraft_ui/js/artifact/ArtifactTrait.js
+++ shadowcraft_ui/js/artifact/ArtifactTrait.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class ArtifactTrait extends React.Component {
+class ArtifactTrait extends React.Component {
     constructor(props) {
         super(props);
+        this.props = props;
         this.handleClick = this.handleClick.bind(this);
         this.handleRightClick = this.handleRightClick.bind(this);
     }
@@ -18,13 +20,53 @@ export default class ArtifactTrait extends React.Component {
     }
 
     render() {
+        
         return (
-            <div className="trait" id={this.props.id} data-max-rank={this.props.max_rank} style={{ left: this.props.left, top: this.props.top }} onClick={this.handleClick} onContextMenu={this.handleRightClick} data-tooltip-href={`http://www.wowdb.com/spells/${this.props.tooltip_id}?artifactRank=${this.props.cur_rank}&artifactMaxRank=${this.props.max_rank}`}>
+            <div
+                className="trait"
+                id={this.props.id}
+                data-max-rank={this.props.max_rank}
+                style={{
+                    left: this.props.left,
+                    top: this.props.top
+                }}
+                onClick={this.handleClick}
+                onContextMenu={this.handleRightClick}
+                data-tooltip-href={
+                    `http://www.wowdb.com/spells/${this.props.tooltip_id}` +
+                    `?artifactRank=${this.props.cur_rank}` +
+                    `&artifactMaxRank=${this.props.max_rank}`
+                }
+            >
                 <img className="relic inactive" src="/static/images/artifacts/relic-blood.png" />
-                <img className={'icon' + (this.props.enabled ? '' : ' inactive')} src={'http://wow.zamimg.com/images/wow/icons/large/' + this.props.icon + '.jpg'} data-tooltip-href={`http://www.wowdb.com/spells/${this.props.id}`} />
-                <img className="ring" src={'/static/images/artifacts/ring-' + this.props.ring + '.png'} />
-                <span className={'level' + (this.props.enabled ? '' : ' inactive')}>{this.props.cur_rank}/{this.props.max_rank}</span>
+                <img
+                    className={'icon' + (this.props.enabled ? '' : ' inactive')}
+                    src={`http://wow.zamimg.com/images/wow/icons/large/${this.props.icon}.jpg`}
+                    data-tooltip-href={`http://www.wowdb.com/spells/${this.props.id}`}
+                />
+                <img className="ring" src={`/static/images/artifacts/ring-${this.props.ring}.png`} />
+                <span className={'level' + (this.props.enabled ? '' : ' inactive')}>
+                    {this.props.cur_rank}/{this.props.max_rank}
+                </span>
             </div>
         );
     }
 }
+
+ArtifactTrait.propTypes = {
+    parent: PropTypes.shape({
+        increase_rank: PropTypes.func.isRequired,
+        decrease_rank: PropTypes.func.isRequired,
+    }).isRequired,
+    tooltip_id: PropTypes.number.isRequired,
+    enabled: PropTypes.bool.isRequired,
+    id: PropTypes.string.isRequired,
+    max_rank: PropTypes.number.isRequired,
+    ring: PropTypes.string.isRequired,
+    cur_rank: PropTypes.number.isRequired,
+    left: PropTypes.string.isRequired,
+    top: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+};
+
+export default ArtifactTrait;
