8c8bc7f4a561d154ce8c842222daedda6644e081
Tim Wojtulewicz <timwoj@ieee.org>
2017-10-06 19:57:25 -0700

    Immutable version of history reducer

--- shadowcraft_ui/js/reducers/historyReducer.js
+++ shadowcraft_ui/js/reducers/historyReducer.js
@@ -1,3 +1,5 @@
+import Immutable from 'immutable';
+
 export const initialHistoryState = {
     dps: [],
     data: []
@@ -9,24 +11,31 @@ export const historyActionTypes = {
 };
 
 export const historyReducer = function (state = initialHistoryState, action) {
+
+    state = Immutable.fromJS(state);
+
     switch (action.type) {
 
         case historyActionTypes.CLEAR_HISTORY: {
-            return Object.assign({}, state, { dps: [], data: [] });
+            return state.set('dps', []).set('data', []).toJS();
         }
 
         case historyActionTypes.ADD_HISTORY: {
-            let newState = Object.assign({}, state);
-            newState.dps.push(Math.round(action.dps * 10.0) / 10.0);
-            newState.data.push(
+
+            let dps = state.get('dps');
+            dps = dps.concat(Math.round(action.dps * 10.0) / 10.0);
+
+            let data = state.get('data');
+            data = data.concat(
                 {
                     character: JSON.parse(JSON.stringify(action.character)),
                     settings: Object.assign({}, action.settings)
                 }
             );
-            return newState;
+
+            return state.set('dps', dps).set('data', data).toJS();
         }
     }
 
-    return state;
+    return state.toJS();
 };
