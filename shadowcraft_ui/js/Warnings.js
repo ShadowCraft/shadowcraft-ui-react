df936c01cf5293d159493b680b30aea17514d354
Tim Wojtulewicz <timwoj@ieee.org>
2017-11-09 09:32:01 -0700

    Warn on items missing from the database. Fixes #52

--- shadowcraft_ui/js/Warnings.js
+++ shadowcraft_ui/js/Warnings.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import Gear from './viewModels/Gear';
-import { List } from 'immutable';
 import { connect } from 'react-redux';
+
+import Gear from './viewModels/Gear';
 import store from './store';
+import { ITEM_DATA } from './item_data';
 
 class Warnings extends React.Component {
 
@@ -18,6 +19,17 @@ class Warnings extends React.Component {
 
         this.props.gear.keySeq().forEach(slot => {
             let item = this.props.gear.get(slot);
+
+            let dbItems = ITEM_DATA.filter(function(dbItem) {
+                return item.id == dbItem.id;
+            });
+
+            if (dbItems.length == 0) {
+                let quality = `quality-${item.get('quality')}`;
+                warnings.push(<div className="log warning" key={keyIndex}><span key={`${item.get('name')}enchant`} className={quality}>{item.get('name')}</span> is not in the database, and may not function as expected. Please notify the Shadowcraft devs!</div>);
+                keyIndex = keyIndex + 1;
+            }
+
             if (slot == "neck" || slot == "finger1" || slot == "finger2" || slot == "back") {
                 if (item.get('enchant') == 0) {
                     let quality = `quality-${item.get('quality')}`;
