ed857748b3ec311990a23e909c13ffe728807505
wavefunctionp <m.corey.thompson@gmail.com>
2017-11-14 10:15:17 -0600

    Remove improper use of Item from createItem.test

--- shadowcraft_ui/js/items/createItem.test.js
+++ shadowcraft_ui/js/items/createItem.test.js
@@ -1,14 +1,13 @@
 import { createItem } from './createItem';
-import Item from '../viewModels/Item';
 
 describe('createItem', () => {
-    const initItem = new Item({
+    const initItem = {
         item_level: 885,
         stats:{
             agility: 1886,
             haste: 982,
         }
-    });
+    };
     it('should create an item with the correct ilvl', () => {
         const item = createItem(initItem, 1000);
         expect(item.item_level).toEqual(1000);
@@ -16,11 +15,11 @@ describe('createItem', () => {
 
     it('should have the primary stat', ()=>{
         const item = createItem(initItem, 930);
-        expect(item.stats.get('agility')).toEqual(2868); //really supposed to be 2870
+        expect(item.stats.agility).toEqual(2868); //really supposed to be 2870
     });
 
     it('should have the correct secondary stats', ()=>{
         const item = createItem(initItem, 930);
-        expect(item.stats.get('haste')).toEqual(1162);
+        expect(item.stats.haste).toEqual(1162);
     });
 });
