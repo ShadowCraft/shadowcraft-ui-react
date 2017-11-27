8c0f2388e995e7d38f28aa1e03f54d5997760252
Tim Wojtulewicz <timwoj@ieee.org>
2017-11-26 20:30:14 -0700

    Link in TOV, EN, and dungeon items to the index

--- shadowcraft_ui/js/items/index.js
+++ shadowcraft_ui/js/items/index.js
@@ -1,6 +1,9 @@
 import { getTOSItems } from './TOSItems';
 import { getNHItems } from './Nighthold';
 import { getLegionCraftedItems } from './legionCraftedItems';
+import { getDungeonItems } from './dungeonItems';
+import { getENItems } from './EmeraldNightmare';
+import { getTOVItems } from './TOVItems';
 
 // we can just register the different definitions here (the multiple TOS entrys are just an example, they would all be different)
 // we can also impliment any caching or local storage stategies here
@@ -9,5 +12,8 @@ export function getItems(slot = 'head', min = 0, max = 10000, currentIlvl) {
         ...getTOSItems(slot, min, max), // important to spread into this array, not just assign
         ...getNHItems(slot, min, max),
         ...getLegionCraftedItems(slot, currentIlvl),
+        ...getDungeonItems(slot, currentIlvl),
+        ...getENItems(slot, currentIlvl),
+        ...getTOVItems(slot, currentIlvl),
     ];
 } 
