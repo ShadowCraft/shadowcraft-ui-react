3624e60615ebc36497f90f443a1a5f74315fa98c
Tim Wojtulewicz <timwoj@ieee.org>
2017-12-26 18:46:22 -0700

    Changes to generate_static_items: - trim rand_prop_points down to ilvls >= 750 - rework item_sparse and rand_prop_points to be objects instead of straight arrays - add weapon stats to item_sparse

--- utils/generate_static_items.js
+++ utils/generate_static_items.js
@@ -1,7 +1,7 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var fs = require('fs');
-var parse = require('csv-parse/lib/sync');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
 const crypto = require('crypto');
 
 let url = 'mongodb://localhost:27017/roguesim_python';
@@ -21,7 +21,7 @@ MongoClient.connect(url, function(err, db) {
         wstream.write(`export const CHARACTER_DATA_VERSION='${md5}';`);
 
         wstream.write('export const ITEM_DATA=[');
-        let len = docs.length;
+        const len = docs.length;
         let database_ids = [];
         for (let i = 0; i < len; i++) {
             delete docs[i]['_id'];
@@ -37,7 +37,7 @@ MongoClient.connect(url, function(err, db) {
 
         // Load the description data and find all of the entries where the name starts with "of the".
         // These are the random properties. If we find that we missed one later, we can add it here.
-        let descData = fs.readFileSync('shadowcraft_ui/external_data/ItemNameDescription.dbc.csv');
+        const descData = fs.readFileSync('shadowcraft_ui/external_data/ItemNameDescription.dbc.csv');
         let descriptions = {};
         parse(descData).filter(function(row) {
             return row[1].startsWith("of the");
@@ -48,8 +48,8 @@ MongoClient.connect(url, function(err, db) {
         // Get a list of bonus IDs where the val_1 column is in the list of descriptions above.
         // This is how we find a list of bonus IDs that map to a description. Skip any that are
         // less than 1000 since those likely don't mean anything to Legion items.
-        let data = fs.readFileSync('shadowcraft_ui/external_data/ItemBonus.dbc.csv');
-        let allBonuses = parse(data);
+        const data = fs.readFileSync('shadowcraft_ui/external_data/ItemBonus.dbc.csv');
+        const allBonuses = parse(data);
         let descrMap = {};
         allBonuses.filter(function(row) {
             return row[1] in descriptions && parseInt(row[3]) > 1000;
@@ -100,30 +100,62 @@ MongoClient.connect(url, function(err, db) {
         // Write out the entirety of the RandPropPoints table since we don't know what
         // ilvl or slot the item is ahead of time. Take out the header row since it's
         // useless and sort the data by the first item.
-        let randPropData = fs.readFileSync('shadowcraft_ui/external_data/RandPropPoints.dbc.csv');
+        const randPropData = fs.readFileSync('shadowcraft_ui/external_data/RandPropPoints.dbc.csv');
         let randProps = {};
-        parse(randPropData).filter(function(row) {
-            return row[0] != 'id';
+        parse(randPropData, {columns: true}).filter(function(row) {
+            return row['id'] >= 750;
         }).map(function(row) {
-            randProps[row[0]] = row;
+            let data = {epic: [], rare: [], uncommon: []};
+            for (let i = 0; i < 5; i++) {
+                data['epic'][i] = row[`epic_points_${i+1}`];
+                data['rare'][i] = row[`rare_points_${i+1}`];
+                data['uncommon'][i] = row[`uncm_points_${i+1}`];
+            }
+            randProps[row['id']] = data;
         });
 
         wstream.write("export const RAND_PROP_POINTS=");
         wstream.write(JSON.stringify(randProps));
         wstream.write(';');
 
+        const itemDamageData = fs.readFileSync('shadowcraft_ui/external_data/ItemDamageOneHand.dbc.csv');
+        let itemDamage = {};
+        parse(itemDamageData, {columns: true}).filter(function(row) {
+            return row['ilevel'] >= 750;
+        }).map(function(row) {
+            itemDamage[row['ilevel']] = row;
+        });
+
         // Write out the sparse item data to use to calculate item variants in
         // combination with the props data above. Trim the rows down to just the rows
         // that we need though so we're not sending a shit load of extra data along
         // with it.
-        // TODO: trimming
-        let itemSparseData = fs.readFileSync('shadowcraft_ui/external_data/ItemSparse.dbc.csv');
+        const itemSparseData = fs.readFileSync('shadowcraft_ui/external_data/ItemSparse.dbc.csv');
         let itemSparse = {};
-        parse(itemSparseData).filter(function(row) {
-            return row[0] != 'id' && database_ids.indexOf(parseInt(row[0])) != -1;
+        parse(itemSparseData, {columns: true}).filter(function(row) {
+            return row[0] != 'id' && database_ids.indexOf(parseInt(row['id'])) != -1;
         }.bind(database_ids)).map(function(row) {
-            itemSparse[row[0]] = row;
-        });
+            let data = {stat_alloc: [], stat_val: [], stat_type: []};
+            for (let i = 0; i < 10; i++) {
+                data['stat_alloc'][i] = row[`stat_alloc_${i+1}`];
+                data['stat_val'][i] = row[`stat_val_${i+1}`];
+                data['stat_type'][i] = row[`stat_type_${i+1}`];
+            }
+
+            // If this item is a weapon, also include the damage stats.
+            if (row['inv_type'] == 21 || row['inv_type'] == 22) {
+                const speed = row['delay'] / 1000;
+                const dps = itemDamage[row['ilevel']][`v_${row['quality']}`];
+                data['weapon'] = {
+                    speed: speed,
+                    dps: dps,
+                    min_dmg: Math.round(dps * speed * (1.0 - row['dmg_range'] / 2.0)),
+                    max_dmg: Math.round(dps * speed * (1.5 - row['dmg_range'] / 2.0))
+                }
+            }
+
+            itemSparse[row['id']] = data;
+        }.bind(itemDamage));
         wstream.write("export const ITEM_SPARSE=");
         wstream.write(JSON.stringify(itemSparse));
         wstream.write(';');
