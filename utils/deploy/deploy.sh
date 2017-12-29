d1f92b7a2509ef4615fbb1df0f8bee158ac667ed
Tim Wojtulewicz <timwoj@ieee.org>
2017-12-29 11:19:47 -0700

    Updating deploy files to match current environment

--- utils/deploy/deploy.sh
+++ utils/deploy/deploy.sh
@@ -3,4 +3,4 @@
 cp shcreact.nginx /etc/nginx/sites-available/shcreact.conf
 ln -s /etc/nginx/sites-available/shcreact.conf /etc/nginx/sites-enabled/shcreact.conf
 cp shcreact.supd /etc/supervisor/conf.d/shcreact.conf
-cp shcreact.tmpfiles /etc/tmpfiles.d/shcreact.conf
+cp shcreact.sudoers /etc/sudoers.d/shcreact.conf
