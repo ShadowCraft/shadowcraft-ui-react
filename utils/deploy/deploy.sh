b7c24813116fb0a84a5f3979649b3a8cc7007599
Tim Wojtulewicz <timwoj@ieee.org>
2017-12-29 11:21:52 -0700

    Force creation of nginx symlink to avoid warning

--- utils/deploy/deploy.sh
+++ utils/deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp shcreact.nginx /etc/nginx/sites-available/shcreact.conf
-ln -s /etc/nginx/sites-available/shcreact.conf /etc/nginx/sites-enabled/shcreact.conf
+ln -sf /etc/nginx/sites-available/shcreact.conf /etc/nginx/sites-enabled/shcreact.conf
 cp shcreact.supd /etc/supervisor/conf.d/shcreact.conf
 cp shcreact.sudoers /etc/sudoers.d/shcreact.conf
