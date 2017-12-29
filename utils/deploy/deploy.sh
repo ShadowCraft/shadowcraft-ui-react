2156eef157313e49087b395fd474b10a6c5f183d
Tim Wojtulewicz <timwoj@ieee.org>
2017-12-29 11:33:53 -0700

    Create venv as part of initial deploy script

--- utils/deploy/deploy.sh
+++ utils/deploy/deploy.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-cp shcreact.nginx /etc/nginx/sites-available/shcreact.conf
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
+cp ${SCRIPT_DIR}/shcreact.nginx /etc/nginx/sites-available/shcreact.conf
 ln -sf /etc/nginx/sites-available/shcreact.conf /etc/nginx/sites-enabled/shcreact.conf
-cp shcreact.supd /etc/supervisor/conf.d/shcreact.conf
-cp shcreact.sudoers /etc/sudoers.d/shcreact.conf
+cp ${SCRIPT_DIR}/shcreact.supd /etc/supervisor/conf.d/shcreact.conf
+cp ${SCRIPT_DIR}/shcreact.sudoers /etc/sudoers.d/shcreact.conf
+
+pushd ${SCRIPT_DIR}/../.. > /dev/null
+if [ ! -d venv ]; then
+    virtualenv -p python3 venv
+fi
+popd > /dev/null
