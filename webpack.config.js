40dfbb1fa460b1ed259207e529caa61c95e6af49
Tim Wojtulewicz <timwoj@ieee.org>
2018-01-11 21:39:34 -0700

    Move WebpackShellPlugin to production only. Fixes #132

--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,6 @@ var pluginsWebpack = [
     }),
     new webpack.DefinePlugin({
         __COMMIT_HASH__: JSON.stringify(gitPlugin.version())
-    }),
-
-    // This will rm anything except the last four bundles and CSS builds so that we
-    // don't fill up the disk full of crap.
-    new WebpackShellPlugin({
-        onBuildStart: ['find shadowcraft_ui/static -name "bundle-*.js" -printf "%Ts\t%p\n" | sort -nr | cut -f2 | tail -n +5 | xargs rm -f',
-                       'find shadowcraft_ui/static/css -name "main-*.css" -printf "%Ts\t%p\n" | sort -nr | cut -f2 | tail -n +5 | xargs rm -f'],
-        safe: true
     })
 ];
 
@@ -32,7 +24,15 @@ if ('production' === process.env.NODE_ENV) {
     var prodEnv = [
         new webpack.optimize.OccurrenceOrderPlugin(true),
         new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.DedupePlugin()
+        new webpack.optimize.DedupePlugin(),
+
+        // This will rm anything except the last four bundles and CSS builds so that we
+        // don't fill up the disk full of crap.
+        new WebpackShellPlugin({
+            onBuildStart: ['find shadowcraft_ui/static -name "bundle-*.js" -printf "%Ts\t%p\n" | sort -nr | cut -f2 | tail -n +5 | xargs rm -f',
+                           'find shadowcraft_ui/static/css -name "main-*.css" -printf "%Ts\t%p\n" | sort -nr | cut -f2 | tail -n +5 | xargs rm -f'],
+            safe: true
+        })
     ];
 
     pluginsWebpack.concat(pluginsWebpack, prodEnv);
