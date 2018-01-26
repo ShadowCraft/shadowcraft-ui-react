2526ba0ca8b93d14b30efa3bd19bb6ae50ce462b
Tim Wojtulewicz <timwoj@ieee.org>
2018-01-26 16:50:25 -0700

    Fixed loading talents from wowhead, and a bit of other rework to the talent generation script

--- utils/data_generation/generate_talents.py
+++ utils/data_generation/generate_talents.py
@@ -24,121 +24,8 @@ from bs4 import BeautifulSoup
 
 BLIZZARD_API_KEY = 'wwa5krsuf4m4yzuu7qtrd42natfu2an2'
 
-if sys.argv[1] == '--wowhead':
-
-    # This function is used at the end to make generating the output for javascript more
-    # generic. This takes the element from BeautifulSoup and scrapes the talent data for
-    # any given spec.
-    def build(source, letter):
-        talents = source.select('div[cursor="pointer"]')
-        for t in talents:
-            tier = t['data-row']
-
-            # Talents are indexed starting at 1 in the engine. Changing them to be the same here
-            # makes a lot of life easier in the UI code.
-            column = t['data-col'] + 1
-
-            spell = t.select('a[class="screen"]')[0]['href'].split('=')[1]
-            name = t.select('td')[0].text
-
-            icon = t.select('ins')[0].get('style')
-            icon = icon.split('/')[-1]
-            icon = icon.split('.jpg')[0]
-            print('        self.talents[:%s] << {tier: %d, column: %d, spell: %d, name: "%s", icon: "%s"}' % (letter, int(tier), int(column), int(spell), name, icon))
-
-    dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = (
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
-        "(KHTML, like Gecko) Chrome/15.0.87"
-    )
-
-    # Create a PhantomJS web driver to load the pages including executing all of
-    # the javascript on the page.
-    browser = webdriver.PhantomJS(desired_capabilities=dcap)
-
-    # Scrape the three talent trees into strings and use BeautifulSoup to grab
-    # just the element we want from each of them.
-    browser.get('http://legion.wowhead.com/talent-calc/rogue/assassination')
-    time.sleep(2)
-    source = browser.page_source
-    soup = BeautifulSoup(source, 'html.parser')
-    assassination = soup.find('div', class_='talentcalc-core')
-
-    browser.get('http://legion.wowhead.com/talent-calc/rogue/outlaw')
-    time.sleep(2)
-    source = browser.page_source
-    soup = BeautifulSoup(source, 'html.parser')
-    outlaw = soup.find('div', class_='talentcalc-core')
-
-    browser.get('http://legion.wowhead.com/talent-calc/rogue/subtlety')
-    time.sleep(2)
-    source = browser.page_source
-    soup = BeautifulSoup(source, 'html.parser')
-    subtlety = soup.find('div', class_='talentcalc-core')
-    browser.quit()
-
-    build(assassination, 'a')
-    build(outlaw, 'Z')
-    build(subtlety, 'b')
-
-elif sys.argv[1] == '--blizzard':
-
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-    params = {'apikey': BLIZZARD_API_KEY,
-              'locale': 'en_US'}
-
-    url = 'https://us.api.battle.net/wow/data/talents'
-    resp = requests.get(url, params=params, timeout=7, headers=headers)
-    json = resp.json()
-    rogue_talents = json['4']
-
-    talent_data = {}
-    for spec in rogue_talents['specs']:
-        talent_data[spec['order']] = {
-            'name': spec['name'],
-            'icon': spec['icon'],
-            'talents': []
-        }
-
-    for row_index, row in enumerate(rogue_talents['talents']):
-        for col_index, col_talents in enumerate(row):
-            order0 = None
-            order1 = None
-            order2 = None
-
-            for spec_index, spec_talent in enumerate(col_talents):
-                t = {
-                    'row': spec_talent['tier'],
-                    'col': spec_talent['column'],
-                    'name': spec_talent['spell']['name'],
-                    'icon': spec_talent['spell']['icon'],
-                    'id': spec_talent['spell']['id']
-                }
-
-                # Talents are indexed starting at 1 in the engine. Changing them to be the same here
-                # makes a lot of life easier in the UI code.
-                t['col'] += 1
-
-                if 'spec' in spec_talent:
-                    if spec_talent['spec']['order'] == 0:
-                        order0 = t
-                    elif spec_talent['spec']['order'] == 1:
-                        order1 = t
-                    elif spec_talent['spec']['order'] == 2:
-                        order2 = t
-                else:
-                    if order0 == None:
-                        order0 = t
-                    if order1 == None:
-                        order1 = t
-                    if order2 == None:
-                        order2 = t
-
-            talent_data[0]['talents'].append(order0)
-            talent_data[1]['talents'].append(order1)
-            talent_data[2]['talents'].append(order2)
-
-    tiers = ['Tier 15','Tier 30','Tier 45','Tier 60','Tier 75','Tier 90','Tier 100']
+def output_data(talent_data):
+    tiers = ['Tier 15', 'Tier 30', 'Tier 45', 'Tier 60', 'Tier 75', 'Tier 90', 'Tier 100']
     specs = ['assassination', 'outlaw', 'subtlety']
     for spec in talent_data:
         data = talent_data[spec]
@@ -147,7 +34,7 @@ elif sys.argv[1] == '--blizzard':
         print('    icon: \'%s\',' % data['icon'])
         print('    talents: [')
 
-        data['talents'] = sorted(data['talents'], key = lambda x: (x['row'], x['col']))
+        data['talents'] = sorted(data['talents'], key=lambda x: (x['row'], x['col']))
         for talent_idx, talent in enumerate(data['talents']):
             print('        {')
             print('            row: %d,' % talent['row'])
@@ -184,3 +71,140 @@ elif sys.argv[1] == '--blizzard':
                 print('    }')
         print('];')
         print()
+
+# This function is used at the end to make generating the output for javascript more
+# generic. This takes the element from BeautifulSoup and scrapes the talent data for
+# any given spec.
+def build_wowhead(source, name, spec_icon):
+    talent_data = {
+        'name': name,
+        'icon': spec_icon,
+        'talents': [],
+    }
+
+    talents = source.select('div[cursor="pointer"]')
+    for talent in talents:
+
+        t = {
+            'row': int(talent['data-row']),
+            'col': int(talent['data-col']),
+            'name': talent.select('td')[0].text,
+            'icon': '',
+            'id': int(talent.select('a[class="screen"]')[0]['href'].split('=')[1])
+        }
+
+        # Talents are indexed starting at 1 in the engine. Changing them to be the same here
+        # makes a lot of life easier in the UI code.
+        t['col'] += 1
+
+        icon = talent.select('ins')[0].get('style')
+        icon = icon.split('/')[-1]
+        icon = icon.split('.jpg')[0]
+        t['icon'] = icon
+
+        talent_data['talents'].append(t)
+    return talent_data
+
+def load_data(source):
+    if source == '--wowhead':
+
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
+            "(KHTML, like Gecko) Chrome/15.0.87"
+        )
+
+        # Create a PhantomJS web driver to load the pages including executing all of
+        # the javascript on the page.
+        browser = webdriver.PhantomJS(desired_capabilities=dcap)
+
+        # Scrape the three talent trees into strings and use BeautifulSoup to grab
+        # just the element we want from each of them.
+        browser.get('http://bfa.wowhead.com/talent-calc/rogue/assassination')
+        time.sleep(2)
+        source = browser.page_source
+        soup = BeautifulSoup(source, 'html.parser')
+        assassination = soup.find('div', class_='talentcalc-core')
+
+        browser.get('http://bfa.wowhead.com/talent-calc/rogue/outlaw')
+        time.sleep(2)
+        source = browser.page_source
+        soup = BeautifulSoup(source, 'html.parser')
+        outlaw = soup.find('div', class_='talentcalc-core')
+
+        browser.get('http://bfa.wowhead.com/talent-calc/rogue/subtlety')
+        time.sleep(2)
+        source = browser.page_source
+        soup = BeautifulSoup(source, 'html.parser')
+        subtlety = soup.find('div', class_='talentcalc-core')
+        browser.quit()
+
+        talent_data = {
+            0: build_wowhead(assassination, 'Assassination', 'ability_rogue_deadlybrew'),
+            1: build_wowhead(outlaw, 'Outlaw', 'inv_sword_30'),
+            2: build_wowhead(subtlety, 'Subtlety', 'ability_stealth')
+        }
+
+        return talent_data
+
+    elif source == '--blizzard':
+
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+        params = {'apikey': BLIZZARD_API_KEY,
+                  'locale': 'en_US'}
+
+        url = 'https://us.api.battle.net/wow/data/talents'
+        resp = requests.get(url, params=params, timeout=7, headers=headers)
+        json = resp.json()
+        rogue_talents = json['4']
+
+        talent_data = {}
+        for spec in rogue_talents['specs']:
+            talent_data[spec['order']] = {
+                'name': spec['name'],
+                'icon': spec['icon'],
+                'talents': []
+            }
+
+        for row_index, row in enumerate(rogue_talents['talents']):
+            for col_index, col_talents in enumerate(row):
+                order0 = None
+                order1 = None
+                order2 = None
+
+                for spec_index, spec_talent in enumerate(col_talents):
+                    t = {
+                        'row': spec_talent['tier'],
+                        'col': spec_talent['column'],
+                        'name': spec_talent['spell']['name'],
+                        'icon': spec_talent['spell']['icon'],
+                        'id': spec_talent['spell']['id']
+                    }
+
+                    # Talents are indexed starting at 1 in the engine. Changing them to be the same here
+                    # makes a lot of life easier in the UI code.
+                    t['col'] += 1
+
+                    if 'spec' in spec_talent:
+                        if spec_talent['spec']['order'] == 0:
+                            order0 = t
+                        elif spec_talent['spec']['order'] == 1:
+                            order1 = t
+                        elif spec_talent['spec']['order'] == 2:
+                            order2 = t
+                    else:
+                        if order0 == None:
+                            order0 = t
+                        if order1 == None:
+                            order1 = t
+                        if order2 == None:
+                            order2 = t
+
+                talent_data[0]['talents'].append(order0)
+                talent_data[1]['talents'].append(order1)
+                talent_data[2]['talents'].append(order2)
+
+        return talent_data
+
+data = load_data(sys.argv[1])
+output_data(data)
